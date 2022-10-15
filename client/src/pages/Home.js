210216f6b34085d4529228628601919478174ac0
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-10-15 11:55:43 -0500

    it works office hours helped

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,7 +7,7 @@ import { QUERY_THOUGHTS } from '../utils/queries';
 const Home = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);
   const thoughts = data?.thoughts || [];
-  console.log(thoughts)
+  console.dir(thoughts)
   return (
     <main>
       <div className="flex-row justify-space-between">
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -30,7 +30,13 @@ const resolvers = {
             .populate('friends')
             .populate('thoughts');
         },
-        
+        thoughts: async (parent, { username }) => {
+          const params = username ? { username } : {};
+          return Thought.find(params).sort({ createdAt: -1 });
+        },
+        thought: async (parent, { _id }) => {
+          return Thought.findOne({ _id });
+        }
 
     },
 
