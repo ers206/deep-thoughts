5d3f574b6ceae4d64fb3c750501f076a67d7ed63
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-10-16 11:26:19 -0500

    profile/usserid not working...done with 21.5

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
@@ -20,11 +22,20 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
@@ -34,41 +45,30 @@ function App() {
           <Header />
           <div className="container">
             <Routes>
-              <Route
-                path="/"
-                element={<Home />}
+              <Route 
+                path="/" 
+                element={<Home />} 
               />
-              <Route
-                path="/login"
-                element={<Login />}
+              <Route 
+                path="/login" 
+                element={<Login />} 
               />
-              <Route
-                path="/signup"
-                element={<Signup />}
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
               />
-              <Route
-                path="/profile"
-                element={<Profile />}
+              <Route 
+                path="/profile" 
+                element={<Profile />} 
               />
-              <Route
-                path="/thought"
-                element={<SingleThought />}
+              <Route 
+                path="/thought/:id" 
+                element={<SingleThought />} 
               />
-          
-                <Route path="/profile">
-                  <Route path=":username" element={<Profile />} />
-                  <Route path="" element={<Profile />} />
-                </Route>
-                <Route
-                  path="/thought/:id"
-                  element={<SingleThought />}
-                />
-              
-              <Route
-                path="*"
-                element={<NoMatch />}
+              <Route 
+                path="*" 
+                element={<NoMatch />} 
               />
-
             </Routes>
           </div>
           <Footer />
